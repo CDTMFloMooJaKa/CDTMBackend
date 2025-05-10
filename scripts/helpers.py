29b0365a0e5cd294e6d4e02add16a8ff7abfafd6
mekadmini <M.Mekadmini@campus.lmu.de>
2025-05-10 23:56:09 +0200

    update helpers

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -1,52 +1,40 @@
 import pandas as pd
 from mistral.mistral_api import get_mistral_response
 
-def convert_df_to_json_for_sector(df, sector, percentage_sold_total, percentage_bought_total):
+def convert_df_to_json_for_sector(df, sector, percentage, action):
     df = df[df["Sector"] == sector]
-    sector_df_buy_max_indices = df.sort_values(by='BuyPct', ascending=False).head(5).index
-    sector_df_sell_max_indices = df.sort_values(by='SellPct', ascending=False).head(5).index
-    indices = list(set(sector_df_buy_max_indices).union(sector_df_sell_max_indices))
+    indices = df.sort_values(by=f'{action}Pct', ascending=False).head(5).index
     companies = []
-
     for i in indices:
         company_dict = {
-            "title": df.at[i, "Name"],
-            "percentage_sold": float(df.at[i, "SellPct"]) / percentage_sold_total,
-            "percentage_bought": float(df.at[i, "BuyPct"]) /percentage_bought_total,
-            "amount_bought": float(df.at[i, "BuyTotal"]),
-            "amount_sold": float(df.at[i, "SellTotal"])
+            "name": df.at[i, "Name"],
+            "percentage": float(df.at[i, f"{action}Pct"]) / percentage
         }
         companies.append(company_dict)
     return companies
 
 
 def convert_df_to_json(df):
-    sector_df = df.groupby("Sector").sum().reset_index().drop("Name", axis=1)
-    sector_df_buy_max_indices = sector_df.sort_values(by='BuyPct', ascending=False).head(5).index
-    sector_df_sell_max_indices = sector_df.sort_values(by='SellPct', ascending=False).head(5).index
-    indices = list(set(sector_df_buy_max_indices).union(sector_df_sell_max_indices))
-    sectors = []
+    sold = convert_type_df_to_json(df, action="Sell")
+    bought = convert_type_df_to_json(df, action="Buy")
+    return {"sold" : sold,
+            "bought" : bought}
 
+def convert_type_df_to_json(df, action="Buy"):
+    sector_df = df.groupby("Sector").sum().reset_index().drop("Name", axis=1)
+    indices = sector_df.sort_values(by=f'{action}Pct', ascending=False).head(5).index
+    sectors = []
     for i in indices:
         sector = sector_df.at[i, "Sector"]
-        #print("sector: ", sector)
-        percentage_sold_total = round(sector_df.at[i, "SellPct"], 4)
-        percentage_bought_total = round(sector_df.at[i, "BuyPct"], 4)
+        percentage = round(sector_df.at[i, f"{action}Pct"], 4)
         sector_dict = {
-            "title": sector,
-            "percentage_sold": percentage_sold_total,
-            "percentage_bought":  percentage_bought_total,
-            "amount_bought":  round(sector_df.at[i, "BuyTotal"],4),
-            "amount_sold": round(sector_df.at[i, "SellTotal"],4),
-            "Elements": convert_df_to_json_for_sector(df, sector, percentage_sold_total, percentage_bought_total)
+            "name":  sector,
+            "percentage": percentage,
+            "subcategories": convert_df_to_json_for_sector(df, sector, percentage, action)
         }
-        #prompt = f"can you please write one short short sentence about the data here in a in a simple way {sector_dict}"
-       # message = get_mistral_response(prompt)
-        sector_dict["message"] = "message"
         sectors.append(sector_dict)
-    #prompt = f"can you please write one short short sentence about the data here in a simple way about the most popular sector {sectors}"
-    #message = get_mistral_response(prompt)
-    return {"sectors" : sectors, "message" : "message"}
+    return sectors
+
 
 
 if __name__ == "__main__":
